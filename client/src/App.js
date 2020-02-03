f27b49aaa1e1257172cc9f7b11e05251f05b0db1
rshaynurov <rshaynurov@c-i-p.ru>
2020-02-03 19:17:27 +0300

    master auth fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavBar } from './components/NavBar';
 import { Loader } from './components/Loader';
 import { useRoutes } from './routes';
-import {login, loginSuccess} from './redux';
+import { loginSuccess } from './redux';
 import { StorageName } from './redux/login/loginTypes';
 import 'materialize-css';
 
 function App() {
+  const [ready, setReady] = useState(false);
+  const loading = useSelector(state => state.login.loading);
   const loginData = useSelector(state => state.login.loginData);
   const dispatch = useDispatch();
 
@@ -16,20 +18,18 @@ function App() {
   const routes = useRoutes(isAuthenticated);
 
   useEffect(() => {
-    dispatch(login());
-    const storageData = JSON.parse(localStorage.getItem(StorageName));
-    if (storageData && storageData.token) {
-      dispatch(loginSuccess(storageData));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
     if (loginData) {
       localStorage.setItem(StorageName, JSON.stringify(loginData));
+    } else {
+      const storageData = JSON.parse(localStorage.getItem(StorageName));
+      if (storageData && storageData.token) {
+        dispatch(loginSuccess(storageData));
+      }
     }
-  }, [loginData]);
+    setReady(true);
+  }, [loginData, dispatch]);
 
-  if (!loginData) {
+  if (!ready || loading) {
     return <Loader />
   }
 
